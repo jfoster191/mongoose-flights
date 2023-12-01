ac6f38f9063765b9b2c56a73250bb1c39b83d156
jfoster <jfoster191@hotmail.com>
2023-11-30 23:56:18 -0500

    adds ticket model; adds new ticket view; adds ticket route and controller with new and create functionality

---
+++ controllers/tickets.js
@@ -0,0 +1,33 @@
+const Ticket = require('../models/ticket');
+const Flight = require('../models/flight');
+
+module.exports = {
+    new: newTicket,
+    create
+}
+
+async function newTicket (req, res){
+    const tickets = await Ticket.find({}).sort('price');
+    const flight = await Flight.findById(req.params.flightId)
+    res.render('tickets/new', {title: 'Add New Ticket', tickets, flight});
+}
+
+async function create (req, res){
+    const URL = req.header('Referer')
+    const id = URL.slice(30,54)
+    try{
+        await Ticket.create(req.body);
+    } catch(err){
+        console.log(err);
+    }
+    res.redirect(`/flights/${id}`);
+    addFlightToTicket(req, res, id);
+}
+
+async function addFlightToTicket(req, res, id){
+    const flightId = await Flight.findById(id)
+    const ticket = await Ticket.find().sort({_id: -1})
+    const lastTicket = ticket[0];
+    lastTicket.flight = flightId;
+    await lastTicket.save();
+}
---
+++ routes/tickets.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const ticketsCtrl = require('../controllers/tickets');
+
+router.get('/flights/:flightId/tickets/new', ticketsCtrl.new);
+
+router.post('/tickets', ticketsCtrl.create);
+
+module.exports = router;
